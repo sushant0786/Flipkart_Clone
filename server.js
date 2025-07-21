3dd3d4dc46407f75de9b7f41057dfdebe4abace1
Sushant Bodade <sushantbodade22@spit.ac.in>
2025-07-21 21:01:55 +0530

    Upload full project

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+require('dotenv').config(); // load .env
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true); // to avoid the warning
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
 const path = require('path');
 const express = require('express');
 const cloudinary = require('cloudinary');
